cd3d18416cf97987ec667f29e9bbd3fc1ca065ae
jkickic <jkickic@gmail.com>
2021-05-24 20:45:58 +0200

    Add a divider, dynamic font size

---
+++ src/components/atoms/Divider.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Divider = () => {
+  return (
+    <div className="h-divider">
+      <div className="shadow" />
+    </div>
+  )
+}
+
+export default Divider
--- src/pages/en.js
+++ src/pages/en.js
@@ -5,6 +5,7 @@ import SEO from "../components/SEO"
 import Header from "../components/Header"
 import Services from "../components/Services"
 import Courses from "../components/Courses"
+import Divider from "../components/atoms/Divider"
 import Service from "../components/Service"
 import Contact from "../components/Contact"
 import data from "../site-data/siteData"
@@ -17,6 +18,7 @@ const EnPage = () => (
     <Header />
     <Services />
     <Contact />
+    <Divider />
     <Courses {...data.courses} />
     {data.services.map((it, idx) => (
       <Service key={idx} {...it} />
